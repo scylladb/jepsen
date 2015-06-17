d535a081f7e4942ab7920636c28cb02f23004e61
Joel Knighton <joel.knighton@datastax.com>
2015-06-17 05:14:30 +0000

    Add support for timestamping report directories.

--- cassandra/test/cassandra/core_test.clj
+++ cassandra/test/cassandra/core_test.clj
@@ -5,35 +5,44 @@
             [jepsen [core :as jepsen]
              [report :as report]]))
 
+(defn date-tag
+  "Date tagging for the report directory"
+  []
+  (->> (java.util.Date.)
+       (.format (java.text.SimpleDateFormat. "yyyy-MM-dd-HH-mm-ss"))))
+
+; Timestamp for this session of testing - either lein test or repl
+(def timestamp (date-tag))
+
 (defn run-set-test!
   "Runs a set test"
-  [test]
+  [test ts]
   (let [test (jepsen/run! test)]
     (or (is (:valid? (:results test)))
         (println (:error (:results test))))
-    (report/to (str "report/" (:name test) "/history.edn")
+    (report/to (str "report-" ts "/" (:name test) "/history.edn")
                (pprint (:history test)))
-    (report/to (str "report/" (:name test) "/set.edn")
+    (report/to (str "report-" ts "/" (:name test) "/set.edn")
                (pprint (:set (:results test))))))
 
 (defn run-counter-test!
   "Runs a counter test"
-  [test]
+  [test ts]
   (let [test (jepsen/run! test)]
     (or (is (:valid? (:results test)))
         (println (:error (:results test))))
-    (report/to (str "report/" (:name test) "/history.edn")
+    (report/to (str "report-" ts "/" (:name test) "/history.edn")
                (pprint (:history test)))
-    (report/to (str "report/" (:name test) "/counter.edn")
+    (report/to (str "report-" ts "/" (:name test) "/counter.edn")
                (pprint (:counter (:results test))))))
 
 (defn run-cas-register-test!
   "Runs a cas register test"
-  [test]
+  [test ts]
   (let [test (jepsen/run! test)]
     (or (is (:valid? (:results test)))
         (println (:error (:results test))))
-    (report/to (str "report/" (:name test) "/history.edn")
+    (report/to (str "report-" ts "/" (:name test) "/history.edn")
                (pprint (:history test)))
-    (report/to (str "report/" (:name test) "/linearizability.txt")
+    (report/to (str "report-" ts "/" (:name test) "/linearizability.txt")
                (-> test :results :linear report/linearizability))))
