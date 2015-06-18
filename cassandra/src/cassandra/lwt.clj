6e131488afd6c8d03d91c833475f00f3fdf93eeb
Joel Knighton <joel.knighton@datastax.com>
2015-06-18 20:34:27 +0000

    Small logging increase in lwt tests.

--- cassandra/src/cassandra/lwt.clj
+++ cassandra/src/cassandra/lwt.clj
@@ -81,10 +81,13 @@
                                            (where [[= :id 0]]))]
                     (if (-> result first ak)
                       (assoc op :type :ok)
-                      (do (cql/insert conn "lwt" {:id 0
-                                                  :value v'}
-                                      (if-not-exists))
-                          (assoc op :type :ok))))
+                      (do
+                        (info "This is our first write, so we need to insert")
+                        (info "This log message should only appear once")
+                        (cql/insert conn "lwt" {:id 0
+                                                :value v'}
+                                    (if-not-exists))
+                        (assoc op :type :ok))))
                   (catch UnavailableException e
                     (assoc op :type :fail :error (.getMessage e)))
                   (catch ReadTimeoutException e
