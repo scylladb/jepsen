03c106e4647eda1aab7f074656119cbfb2d230c4
Aphyr <aphyr@aphyr.com>
2020-09-14 22:09:38 -0400

    conductors: clean up unused bootstrap/decommission conductors.
    we have a nemesis for these now.

--- scylla/src/scylla/conductors.clj
+++ scylla/src/scylla/conductors.clj
@@ -10,40 +10,6 @@
              [client :as client]
              [control :as c]]))
 
-(defn bootstrapper
-  []
-  (reify client/Client
-    (setup! [this test node] this)
-    (invoke! [this test op]
-      (let [bootstrap (:bootstrap test)]
-        (if-let [node (first @bootstrap)]
-          (do (info node "starting bootstrapping")
-              (swap! bootstrap rest)
-              (c/on node (db/start! node test))
-              (while (some #{db/dns-resolve (name node)}
-                           (db/joining-nodes test))
-                (info node "still joining")
-                (Thread/sleep 1000))
-              (assoc op :value (str node " bootstrapped")))
-          (assoc op :value "no nodes left to bootstrap"))))
-    (teardown! [this test] this)))
-
-(defn decommissioner
-  []
-  (reify client/Client
-    (setup! [this test node] this)
-    (invoke! [this test op]
-      (let [decommission (:decommission test)]
-        (if-let [node (some-> test db/live-nodes (set/difference @decommission)
-                              shuffle (get 3))] ; keep at least RF nodes
-          (do (info node "decommissioning")
-              (info @decommission "already decommissioned")
-              (swap! decommission conj node)
-              (db/nodetool node "decommission")
-              (assoc op :value (str node " decommissioned")))
-          (assoc op :value "no nodes eligible for decommission"))))
-    (teardown! [this test] this)))
-
 (defn replayer
   []
   (reify client/Client
